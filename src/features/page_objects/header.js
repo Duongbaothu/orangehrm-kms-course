ab52e45cf322baa8ea3ce8916daeb552f9ee7d36
kietalang710 <84842750+kietalang710@users.noreply.github.com>
2022-11-01 17:26:34 +0700

    SEL-E01S04: Visit all pages from page header (#6)
    * implement testcase for visit all pages from page header
    
    * Add automation test script
    
    * update test script and feature
    
    * Update PR
    
    * update script add waitloading
    
    * update header step definition and page object

--- src/features/page_objects/header.js
+++ src/features/page_objects/header.js
@@ -3,6 +3,11 @@ const common = require('./common');
 
 const btnAccount = `//button[@id='ACCOUNT']`;
 const btnCustomerLogin = `//button[@id='ACCOUNT']/..//a[contains(.,'Customer Login')]`;
+const logoImage = `//div[@class="logo"]//img[@alt="logo"]`;
+const ddlCompany = `//div[@class='main-menu-content w-100']//li[@class='footm']/a[@href='company']`;
+const ddoCompany = `//div[@class='main-menu-content w-100']//li[@class='footm']
+/a[@href='company']/..//a[text()='$optionName']`;
+const lnkPage = `//div[@class='main-menu-content w-100']//*[@href='$href']`;
 
 module.exports = {
   /**
@@ -18,5 +23,40 @@ module.exports = {
   */
   async clickBtnCustomerLogin() {
     await keywords.waitClick.call(this, btnCustomerLogin);
+  },
+
+  /**
+  * Click the Logo image.
+  */
+  async clickLogoImage() {
+    await common.waitLoading.call(this);
+    await keywords.waitClick.call(this, logoImage);
+  },
+
+  /**
+  * Click page link.
+  * @param {String} href The page link.
+  */
+  async clickPageLink(href) {
+    await common.waitLoading.call(this);
+    const lnk = lnkPage.replace(`$href`, href);
+    await keywords.waitClick.call(this, lnk);
+  },
+
+  /**
+  * Hover in Company dropdown list.
+  */
+  async hoverDDLCompany() {
+    await common.waitLoading.call(this);
+    await keywords.moveMouse.call(this, ddlCompany);
+  },
+
+  /**
+  * Select the option in Company dropdown list
+  * @param {String} optionName The option.
+  */
+  async clickCompanyOptions(optionName) {
+    const xpath = ddoCompany.replace(`$optionName`, optionName);
+    await keywords.waitClick.call(this, xpath);
   },
 };
--- src/features/step_definitions/headerSteps.js
+++ src/features/step_definitions/headerSteps.js
@@ -1,6 +1,36 @@
 const {When} = require('@cucumber/cucumber');
 const header = require('../page_objects/header');
+const {filltemplate} = require('@ln-maf/core');
+const fillTemplate = filltemplate;
+
+/**
+ * Returns the value of the variable if it exists in this.results.
+ * @param {string} variable the variable to check
+ * @param {Object} scenario the scenario to check
+ * @return {Object} the value of the variable if it exists in this.results.
+ * Returns the variable itself if variable does not contain "${}"
+ */
+function getVal(variable, scenario) {
+  if (!scenario.results) {
+    scenario.results = {};
+  }
+  return fillTemplate(variable, scenario.results);
+};
 
 When('User click Account button', header.clickBtnAccount);
 
 When('User click Customer Login button', header.clickBtnCustomerLogin);
+
+When('User click Logo image', header.clickLogoImage);
+
+When('User hover Company button', header.hoverDDLCompany);
+
+When('User click {string} option on Company dropdown list', async function(option) {
+  optionName = getVal(option, this);
+  await header.clickCompanyOptions.call(this, optionName);
+});
+
+When('User click {string} page with href is {string}', async function(title, href) {
+  link = getVal(href, this);
+  await header.clickPageLink.call(this, link);
+});
